5bae97ec60d020da382b7fce8a8adc4de8f3e7f7
liam <liamstuff@live.com>
2017-02-12 10:53:17 +1300

    Changes to be committed: 	deleted:    bin/.gitignore 	modified:   bin/Rinstructions.class 	new file:   bin/TestMain.class 	modified:   src/Rinstructions.java

--- src/Rinstructions.java
+++ src/Rinstructions.java
@@ -3,8 +3,19 @@ public class Rinstructions implements instruction {
 	//OP $rd, $rs, $rt
 	
 	String opCode;
-	int sourceRegister;
-	int destinationRegister;
-	int tempRegister;
+	Register sourceRegister; //rs
+	Register sourceRegister2; //rt
+	Register destinationRegister; //rd
+	
+	public Rinstructions(String opcode, Register rd, Register rs, Register rt) {
+		this.opCode = opcode;
+		this.destinationRegister = rd;
+		this.sourceRegister = rs;
+		this.destinationRegister = rt;
+	}
+	
+	public void process() {
+		
+	}
 
-}
+} 
